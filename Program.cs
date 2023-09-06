1d6141f06b0534ed016a7ad80657b771793a1684
Only-Well <wellissonsilvareis28@hotmail.com>
2023-09-06 14:13:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿void IsPalindrome()
+{
+    do
+    {
+        string word = Console.ReadLine()!.ToLower();
+        char[] arr = word.ToCharArray();
+        char[] reverse = arr.Reverse().ToArray();
+        string reverseJoin = string.Join("", reverse);
+
+        if (word.Replace(" ","").Equals(reverseJoin.Replace(" ","")))
+        {
+            Console.WriteLine("true\n");
+        }
+        else
+        {
+            Console.WriteLine("false\n");
+        }
+    } while (1 > 0);
+}
+
+
+IsPalindrome();
